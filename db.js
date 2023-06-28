6451150f45c0927533675bab22f60f0c3a6e03c9
sivasankar967 <sankarsiva6464@gmail.com>
2023-06-28 18:35:41 +0530

    changes

--- db.js
+++ db.js
@@ -2,10 +2,12 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const ObjectID = mongodb.ObjectId;
 
+
 let database;
 
+
 async function getDatabase(){
-    const client = await MongoClient.connect('mongodb://localhost:27017');
+    const client = await MongoClient.connect('mongodb+srv://sankarsiva6464:6384140761@ejswith-node.0gkljfc.mongodb.net/?retryWrites=true&w=majority');
     database = client.db('library');
 
     if (!database) {
